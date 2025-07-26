0550717fd426fde96cc0220b711ca686d4d1ee69
tracy apps <t@thisroad.org>
2025-07-25 21:28:38 -0500

    post template cleanup

--- wp-content/themes/ignite/template-parts/singular/post.php
+++ wp-content/themes/ignite/template-parts/singular/post.php
@@ -6,42 +6,31 @@
  *
  */
 
-$post_type = get_post_type( get_the_ID() );
-if( $post_type === 'post' ) {
-	$post_type = 'blog_post';
-}
 
-$post_class_list = 'singular_post';
+
 if( has_post_thumbnail() ) :
-	$post_class_list .= ' has_featured_image';
-else :
-	$post_class_list .= ' no_image';
-endif;
+		$featured_img_url = get_the_post_thumbnail_url(get_the_ID(),'full');
+		echo '<div class="featured_image">';
+		echo '<img class="fade-bottom" src="' . esc_url( $featured_img_url ) . '" />';
+		echo '<h1 class="post_title">' . wp_kses_post( get_the_title() ) . '</h1>';
+		echo '</div>';
+
+	else :
+		echo '<div class="no_featured_image">';
+		echo '<h1 class="post_title">' . wp_kses_post( get_the_title() ) . '</h1>';
+		echo '</div>';
+	endif;
+
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class( $post_class_list ); ?>>
+<article id="post-<?php the_ID(); ?>">
 	<header class="post_header">
-		<span class="header_post_type_icon content_type <?php esc_html_e( $post_type ); ?>">
-			<svg class="icon-content_type">
-				<use xlink:href="#CONTENT_TYPE_<?php esc_html_e( $post_type ); ?>"></use>
-			</svg>
-			<h6>Blog Post</h6>
-		</span>
+		<
 		<?php the_title( '<h1 class="post_title">', '</h1>' ); ?>
 		<div class="post_date">
 			<?php echo get_the_date('M d, Y'); ?>
 		</div>
-
-		<?php
-		if( has_post_thumbnail() ) :
-			echo '<figure class="featured_image">';
-			the_post_thumbnail( 'large', array( 'class' => 'simple_parallax_scroll' ) );
-			echo '</figure>';
-		endif;
-		?>
-
-		<?php echo ITS_display_related_glossary_term_tags( get_the_ID(), 'blog_post' ); ?>
-
+		
 	</header>
 	<main class="post_main">
 		<?php the_content(); ?>
@@ -50,25 +39,8 @@ endif;
 		<div class="list_of_all_tags">
 			<?php the_tags( '', ' | ', '' ); ?>
 		</div>
-		<svg class="full_width svg_divider">
-			<use xlink:href="#line_divider_option_02" />
-		</svg>
+
 	</footer>
 
 </article>
-<aside id="supplementary" class="additional_content related_content_container">
-
-	<?php
-	ITS_display_related_content( get_the_ID(), 'glossary_terms', 'Related Glossary Terms', 25 );
-	ITS_display_related_content_by_current_tags( get_the_ID(), 'video', 8 );
-	ITS_display_related_content_by_current_tags( get_the_ID(), 'post', 3, 'More Related Blog Posts' );
-
-	ITS_display_related_content( get_the_ID(), 'eduguide', 'Related EduGuides', 5 );
-
-	?>
-
-
-
-
-</aside>
 
